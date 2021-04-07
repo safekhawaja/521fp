6acc03ee501090b15ab5ff95625c14d28ae2e850
Saif <68385788+safekhawaja@users.noreply.github.com>
2021-04-07 14:12:07 -0400

    m

---
+++ make_predictions.m
@@ -0,0 +1,23 @@
+function [predicted_dg] = make_predictions(test_ecog)
+
+% INPUTS: test_ecog - 3 x 1 cell array containing ECoG for each subject, where test_ecog{i} 
+% to the ECoG for subject i. Each cell element contains a N x M testing ECoG,
+% where N is the number of samples and M is the number of EEG channels.
+
+% OUTPUTS: predicted_dg - 3 x 1 cell array, where predicted_dg{i} contains the 
+% data_glove prediction for subject i, which is an N x 5 matrix (for
+% fingers 1:5)
+
+% Run time: The script has to run less than 1 hour. 
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+
+
+
+end
+
